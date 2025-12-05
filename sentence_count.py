e470d8ade5714c795567355800755e06e3e37378
Henry Knieling <henry.knieling@worldiety.de>
2025-12-05 19:37:06 +0100

    sentence counting on .

--- sentence_count.py
+++ sentence_count.py
@@ -2,5 +2,11 @@ def count_sentences(text):
     with open(text, 'r', encoding='utf-8') as file:
         content = file.read()
         print(content)
+    
+    sentences = content.strip().split(".")
+    sentences = [s for s in sentences if s.strip()]
+    
+    return len(sentences)
 
-count_sentences("datei.txt")
+anzahl = count_sentences("datei.txt")
+print(f"Anzahl der Sätze: {anzahl}")
